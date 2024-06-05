98f26ac2cbde288baedabab70afee08e479f409c
AbuZobayed <zobayed.ict05@gmail.com>
2024-06-05 23:44:35 +0600

    same

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,12 +4,7 @@ export default function Contact() {
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
           <img src="/public/contactus.png" alt="" />
-          {/* <h1 className="text-5xl font-bold text-green-600">Contact Us</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p> */}
+         
         </div>
         <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <form className="card-body">
